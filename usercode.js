a33dc86048473b240b9d37996466c5d3e6f8e0e8
Alexander Kozienko <a.s.kozienko@gmail.com>
2020-02-17 14:41:08 +0200

    add js usercode

--- usercode.js
+++ usercode.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 const axios = require('axios');
 
 module.exports = (data) => {
-    moment().format('MMMM Do YYYY, h:mm:ss a');
+    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
     data.foo = 1;
 
