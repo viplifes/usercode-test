364e9b04a10e3c6063242ec3ae0a43e23d88c2de
Alexander Kozienko <a.s.kozienko@gmail.com>
2020-02-21 13:09:09 +0200

    add go usercode

---
+++ usercode.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+        "context"
+
+        "github.com/corezoid/gitcall-go-runner/runner"
+)
+
+func usercode(ctx context.Context, data map[string]interface{}) error {
+
+        data["foo"] = 123
+
+        return nil
+}
+
+func main() {
+        runner.Run(usercode)
+}
+
