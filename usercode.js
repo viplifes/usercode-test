3ce087d2d14f80755b7c6c495316915febdb30ed
Max Kotliar <kotlyar.maksim@gmail.com>
2020-04-09 13:45:22 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -2,9 +2,7 @@ const moment = require('moment');
 const axios = require('axios');
 
 module.exports = (data) => {
-    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-
-    data.foo = 1;
+    data.now = moment().format('MMMM Do YYYY, h:mm:ss a');
 
     return data;
 };
